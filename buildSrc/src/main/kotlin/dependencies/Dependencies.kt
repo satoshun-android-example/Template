0894340182f8c7e242295c751066b4cd51c0282f
SatoShun <shun.sato1@gmail.com>
2020-11-14 06:49:28 +0000

    update

--- buildSrc/src/main/kotlin/dependencies/Dependencies.kt
+++ buildSrc/src/main/kotlin/dependencies/Dependencies.kt
@@ -1,27 +1,27 @@
 package dependencies
 
-const val COMPILE_SDK = 29
+const val COMPILE_SDK = 30
 const val MIN_SDK = 21
-const val TARGET_SDK = 29
+const val TARGET_SDK = 30
 
-private const val VKOTLIN = "1.4.0"
-private const val VNAVIGATION = "2.2.0"
+private const val VKOTLIN = "1.4.10"
+private const val VNAVIGATION = "2.3.1"
 
-const val ANDROID_PLUGIN = "com.android.tools.build:gradle:4.0.1"
+const val ANDROID_PLUGIN = "com.android.tools.build:gradle:4.1.1"
 const val KOTLIN_PLUGIN = "org.jetbrains.kotlin:kotlin-gradle-plugin:$VKOTLIN"
 const val SAFEARGS_PLUGIN = "androidx.navigation:navigation-safe-args-gradle-plugin:$VNAVIGATION"
 const val VERSIONS_PLUGIN = "com.github.ben-manes:gradle-versions-plugin:0.20.0"
 
-private const val VCOUROUTINE = "1.3.4"
+private const val VCOUROUTINE = "1.4.1"
 const val KOTLIN = "org.jetbrains.kotlin:kotlin-stdlib:$VKOTLIN"
 const val COROUTINE = "org.jetbrains.kotlinx:kotlinx-coroutines-core:$VCOUROUTINE"
 const val UI_COROUTINE = "org.jetbrains.kotlinx:kotlinx-coroutines-android:$VCOUROUTINE"
 
 const val KTX = "androidx.core:core-ktx:1.3.1"
-const val ACTIVITYX = "androidx.activity:activity-ktx:1.2.0-alpha08"
-const val FRAGMENTX = "androidx.fragment:fragment-ktx:1.3.0-alpha08"
+const val ACTIVITYX = "androidx.activity:activity-ktx:1.2.0-beta01"
+const val FRAGMENTX = "androidx.fragment:fragment-ktx:1.3.0-beta01"
 
-const val LIFECYCLE_RUNTIME = "androidx.lifecycle:lifecycle-runtime-ktx:2.2.0-alpha04"
+const val LIFECYCLE_RUNTIME = "androidx.lifecycle:lifecycle-runtime-ktx:2.2.0"
 
 const val APPCOMPAT = "androidx.appcompat:appcompat:1.2.0"
 const val RECYCLERVIEW = "androidx.recyclerview:recyclerview:1.1.0"
