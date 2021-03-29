925998a5cad8405feeee410302d601a012f67447
SatoShun <shun.sato1@gmail.com>
2021-03-29 00:50:32 +0000

    use compose

--- app/src/main/java/com/github/satoshun/example/AppActivity.kt
+++ app/src/main/java/com/github/satoshun/example/AppActivity.kt
@@ -1,5 +1,11 @@
 package com.github.satoshun.example
 
+import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
+import androidx.activity.compose.setContent
 
-class AppActivity : AppCompatActivity(R.layout.app_act)
+class AppActivity : AppCompatActivity() {
+  override fun onCreate(savedInstanceState: Bundle?) {
+    super.onCreate(savedInstanceState)
+  }
+}
