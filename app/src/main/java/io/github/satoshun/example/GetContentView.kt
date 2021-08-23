746693d4f9189733aa953271c029ab8879a1ec7b
SatoShun <shun.sato1@gmail.com>
2021-08-23 01:34:14 +0000

    remove unnecessary file

--- app/src/main/java/io/github/satoshun/example/GetContentView.kt
+++
@@ -1,7 +0,0 @@
-package io.github.satoshun.example
-
-import android.app.Activity
-import android.view.ViewGroup
-import androidx.core.view.get
-
-fun Activity.getContentView() = findViewById<ViewGroup>(android.R.id.content)[0]
