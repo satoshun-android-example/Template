68f5a116ec775d382408fa826f56b4ae89ea1366
SatoShun <shun.sato1@gmail.com>
2023-03-11 08:35:47 +0000

    java 17

--- build-logic/convention/src/main/kotlin/com/google/samples/apps/nowinandroid/KotlinAndroid.kt
+++ build-logic/convention/src/main/kotlin/com/google/samples/apps/nowinandroid/KotlinAndroid.kt
@@ -38,12 +38,12 @@ internal fun Project.configureKotlinAndroid(
     }
 
     compileOptions {
-      sourceCompatibility = JavaVersion.VERSION_11
-      targetCompatibility = JavaVersion.VERSION_11
+      sourceCompatibility = JavaVersion.VERSION_17
+      targetCompatibility = JavaVersion.VERSION_17
     }
 
     kotlinOptions {
-      jvmTarget = JavaVersion.VERSION_11.toString()
+      jvmTarget = JavaVersion.VERSION_17.toString()
 
       // Treat all Kotlin warnings as errors (disabled by default)
       // Override by setting warningsAsErrors=true in your ~/.gradle/gradle.properties
